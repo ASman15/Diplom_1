8cdadf39d1003a308df39083d430a39b7af55178
Alexey <asmanzhilevsky@yandex.ru>
2024-08-17 00:12:19 +0500

    add tests

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,2 +1,48 @@
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
+import praktikum.Bun;
+
+
+@RunWith(Parameterized.class)
 public class BunTest {
+
+    private String testName;
+
+    public int testPrice;
+
+    @Mock
+    Bun bun = new Bun(testName, testPrice);
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
+    @Parameterized.Parameters
+    public static Object [][] data() {
+        return new Object [][] {
+                {"black bun", 0},
+                {null, 100},
+        };
+    }
+    public BunTest(String testName, int testPrice) {
+        this.testName=testName;
+        this.testPrice=testPrice;
+    }
+    @Test
+    public void testGetName() {
+        Bun bun = new Bun(testName, testPrice);
+        String returnGetName = bun.getName();
+        Assert.assertEquals(testName, returnGetName);
+    }
+    @Test
+    public void testGetPrice() {
+        Bun bun = new Bun(testName, testPrice);
+        int returnGetPrice = (int) bun.getPrice();
+        Assert.assertEquals(testPrice, returnGetPrice);
+    }
 }
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,2 +1,44 @@
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
 public class IngredientTest {
+    private IngredientType type;
+    private String name;
+    private float price;
+    @Mock
+    private Ingredient ingredient = new Ingredient(type, name, price);
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
+
+    @Test
+    public void testGetTypeSause() {
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, null, 0);
+        IngredientType returnGetType = ingredient.getType();
+        Assert.assertEquals(IngredientType.SAUCE, returnGetType);
+    }
+    @Test
+    public void testGetTypeFilling() {
+        Ingredient ingredient = new Ingredient(IngredientType.FILLING, null, 0);
+        IngredientType returnGetType = ingredient.getType();
+        Assert.assertEquals(IngredientType.FILLING, returnGetType);
+    }
+    @Test
+    public void testGetName() {
+        Ingredient ingredient = new Ingredient(null, "hot sauce", 0);
+        String returnGetName = ingredient.getName();
+        Assert.assertEquals("hot sauce", returnGetName);
+    }
+    @Test
+    public void TestGetPrice() {
+        Ingredient ingredient = new Ingredient(null, null, 100);
+        int returnGetPrice = (int)ingredient.getPrice();
+        Assert.assertEquals(100, returnGetPrice);
+    }
 }
