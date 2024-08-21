2f9541f40e53164bbfc3715aca195d029223b681
Alexey <asmanzhilevsky@yandex.ru>
2024-08-21 10:14:28 +0500

    add IngredientTypeTest

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -1,2 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+@RunWith(Parameterized.class)
 public class IngredientTypeTest {
-}
+    private IngredientType type;
+    private String name;
+    private float price;
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {IngredientType.SAUCE, null, 0},
+                {IngredientType.SAUCE, null, 0},
+        };
+    }
+    public IngredientTypeTest(IngredientType type, String name, int price) {
+        this.type=type;
+        this.name=name;
+        this.price=price;
+    }
+    @Test
+    public void IngredientTypeReturnRightEqual() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        IngredientType returnGetType = ingredient.getType();
+        Assert.assertEquals(type, returnGetType);
+    }
+    }
